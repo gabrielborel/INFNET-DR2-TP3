bc33f3f7d0d0d4364f8c23f611c9bc4cf2d4622d
Gabriel Borel <biel_borel@hotmail.com>
2025-04-07 08:37:43 -0300

    Exercício 2

--- src/main/java/org/INFNET/TP3/Main.java
+++ src/main/java/org/INFNET/TP3/Main.java
@@ -1,12 +1,27 @@
 package org.INFNET.TP3;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
         User usuario = new User("Gabriel Borel", "gabriel.borel@al.infnet.edu.br", "ADMINISTRADOR");
         Task task = new Task("Criação do projeto", "Iniciar o repositório do projeto em questão", Task.Status.TODO, usuario);
+        System.out.println("Task antes de ser atualizada");
         task.exibirDetalhes();
         Task taskAtualizada = task.alterarStatus(Task.Status.IN_PROGRESS);
         System.out.println("---");
+        System.out.println("Task depois de ser atualizada");
         taskAtualizada.exibirDetalhes();
+
+        System.out.println("\n");
+
+        Sprint sprint = new Sprint("Sprint 1", LocalDate.now(), LocalDate.now().plusDays(14), Optional.empty());
+        System.out.println("Sprint antes de ser alteado");
+        sprint.listarTarefas();
+        Sprint sprintAtualizado = sprint.adicionarTarefa(taskAtualizada);
+        System.out.println("---");
+        System.out.println("Sprint depois de alterado");
+        sprintAtualizado.listarTarefas();
     }
 }
---
+++ src/main/java/org/INFNET/TP3/Sprint.java
@@ -0,0 +1,52 @@
+package org.INFNET.TP3;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Sprint {
+    private final String nome;
+    private final LocalDate dataInicio;
+    private final LocalDate dataFim;
+    private final List<Task> tarefas;
+
+    public Sprint(String nome, LocalDate dataInicio, LocalDate dataFim, Optional<List<Task>> tarefas) {
+        this.nome = nome;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+        this.tarefas = tarefas.orElse(new ArrayList<>());
+    }
+
+    public Sprint adicionarTarefa(Task tarefa) {
+        List<Task> tasks = new ArrayList<>(tarefas);
+        tasks.add(tarefa);
+        return new Sprint(nome, dataInicio, dataFim, Optional.of(tasks));
+    }
+
+    public void removerTarefa(Task tarefa) {
+        tarefas.remove(tarefa);
+    }
+
+    public void listarTarefas() {
+        for (Task tarefa : tarefas) {
+            System.out.println(tarefa.getTitulo());
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public List<Task> getTarefas() {
+        return new ArrayList<>(tarefas);
+    }
+}
